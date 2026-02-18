9ba9a1a0bdf71cddb68df23c1f9f702708d8a93a
ashutoshtiwari7612-code <ashutoshtiwari7612@gmail.com>
2026-02-18 14:51:35 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "!ping") {
+    message.reply("🏓 Pong!");
+  }
+});
+
+client.login(process.env.TOKEN);
